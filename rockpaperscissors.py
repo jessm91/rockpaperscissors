4568c67227e27a054345653b8ca414a1eebd0320
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-07 10:51:26 -0500

    added while loop

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,24 +2,31 @@ from random import choice
 
 options = ['rock', 'paper', 'scissors']
 
-answer = input("Please choose rock, paper or scissors: ")
+"""Random computer answer"""
 opponent = choice(options)
 
-if answer == 'rock' and opponent == 'rock':
-    print("Tie! You both chose rock.")
-elif answer == 'rock' and opponent == 'paper':
-    print("You lose! Paper covers rock.")
-elif answer == 'rock' and opponent == 'scissors':
-    print("You win! Rock crushes scissors.")
-elif answer == 'paper' and opponent == 'paper':
-    print("Tie! You both chose paper.")
-elif answer == 'paper' and opponent == 'rock':
-    print("You win! Paper covers rock.")
-elif answer == 'paper' and opponent == 'scissors':
-    print("You lose! Scissors cut paper")
-elif answer == 'scissors' and opponent == 'scissors':
-    print("Tie! You both chose scissors.")
-elif answer == 'scissors' and opponent == 'rock':
-    print("You lose! Rock crushes scissors.")
-elif answer == 'scissors' and opponent == 'paper':
-    print("You win! Scissors cut paper.")
+"""user input & game options"""
+while True:
+    answer = input("Please choose rock, paper or scissors [press 'q' to quit]: ")
+
+    if answer == 'q':
+        break
+
+    if answer == 'rock' and opponent == 'rock':
+        print("Tie! You both chose rock.")
+    elif answer == 'rock' and opponent == 'paper':
+        print("You lose! Paper covers rock.")
+    elif answer == 'rock' and opponent == 'scissors':
+        print("You win! Rock crushes scissors.")
+    elif answer == 'paper' and opponent == 'paper':
+        print("Tie! You both chose paper.")
+    elif answer == 'paper' and opponent == 'rock':
+        print("You win! Paper covers rock.")
+    elif answer == 'paper' and opponent == 'scissors':
+        print("You lose! Scissors cut paper")
+    elif answer == 'scissors' and opponent == 'scissors':
+        print("Tie! You both chose scissors.")
+    elif answer == 'scissors' and opponent == 'rock':
+        print("You lose! Rock crushes scissors.")
+    elif answer == 'scissors' and opponent == 'paper':
+        print("You win! Scissors cut paper.")
